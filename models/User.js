686d7346f1cf2e40f84a79734f1fffdca3ad141a
Rohit Bhetal <96344296+Rohit-Bhetal@users.noreply.github.com>
2024-08-09 14:31:35 +0545

    Models

--- models/User.js
+++ models/User.js
@@ -19,6 +19,59 @@ const userSchema = new Schema(
                     throw new Error('Email is invalid');
                 }
             }
+        },
+        password:{
+            type:String,
+            required:true,
+            minlength:7,
+            trim:true,
+            validate(value){
+                if(value.toLowerCase().includes('password')){
+                    throw new Error('Password cannot')
+                }
+            }
+
+        },
+        age:{
+            type:Number,
+            default:0,
+            validate(value){
+                if(value < 0){
+                    throw new Error('Age must be a positive number')
+                }
+            }
+        },
+        token:[
+            {
+                token:{
+                    type:String,
+                    required:true
+                }
+            }
+        ],
+        avatar:{
+            type:Buffer
         }
+
+    },
+    {
+        timestamps:true
     }
-)
+);
+
+
+userSchema.virtual('tasks',{
+    ref:'Task',
+    localField:'_id',
+    foreignField:'owner'
+});
+
+
+userSchema.methods.toJSON=function(){
+    const userObject = this.toObject();
+    delete userObject.password;
+    delete userObject.tokens;
+    delete userObject.__v;
+    delete userObject.avatar;
+    return userObject;
+}
